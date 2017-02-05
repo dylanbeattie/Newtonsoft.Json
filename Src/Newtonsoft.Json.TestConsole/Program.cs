39a365cd1ff48c6ce4f26c25ac1cfca397c5bc3a
dylanbeattie <dylan@dylanbeattie.net>
2017-02-05 15:53:13 +0000

    Working spike showing how to use a custom converter to override the order of dictionary entries when serializing an ExpandObject

--- Src/Newtonsoft.Json.TestConsole/Program.cs
+++ Src/Newtonsoft.Json.TestConsole/Program.cs
@@ -24,19 +24,81 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Tests;
 using Newtonsoft.Json.Tests.Converters;
 
 namespace Newtonsoft.Json.TestConsole
 {
+
+
+    public class HalPropertyComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Equals(y)) return (0);
+            if (x.Equals("_links")) return (-1); // _links always goes at the start
+            if (y.Equals("_links")) return (1); // _links always goes at the start
+            if (x.Equals("_embedded")) return (1);
+            if (y.Equals("_embedded")) return (-1);
+            return Comparer.Default.Compare(x, y);
+        }
+    }
+
+
+    public class OrderedContractResolver : DefaultContractResolver
+    {
+        private readonly IComparer<string> comparer;
+
+        public OrderedContractResolver(IComparer<string> comparer)
+        {
+            this.comparer = comparer;
+        }
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            return base.CreateProperties(type, memberSerialization).OrderBy(p => p.PropertyName, comparer).ToList();
+        }
+    }
+
+    public class OrderedExpandoObjectConverter : ExpandoObjectConverter
+    {
+        private readonly IComparer<string> comparer;
+
+        public OrderedExpandoObjectConverter(IComparer<string> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var jt = JToken.FromObject(value);
+            if (jt.Type != JTokenType.Object)
+            {
+                jt.WriteTo(writer);
+            }
+            else
+            {
+                var jo = (JObject)jt;
+                writer.WriteStartObject();
+                foreach (var prop in jo.Properties().OrderBy(p => p.Name, comparer)) prop.WriteTo(writer, serializer.Converters.ToArray());
+                writer.WriteEndObject();
+            }
+        }
+
+        public override bool CanWrite => true;
+    }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -45,15 +107,77 @@ namespace Newtonsoft.Json.TestConsole
 
             string version = FileVersionInfo.GetVersionInfo(typeof(JsonConvert).Assembly.Location).FileVersion;
             Console.WriteLine("Json.NET Version: " + version);
-            Console.ReadKey();
-
             Console.WriteLine("Doing stuff...");
+
+
+            var resolver = new DefaultContractResolver();
+            dynamic values = new ExpandoObject();
+            values._embedded = new[] {
+                new { text = "embedded data" },
+                new { text = "should appear" },
+                new { text = "...at the end" }
+            };
+
+            values.charlie = 1;
+            values.bravo = 2;
+            values.alpha = 3;
+            values._links = new { self = new { href = "this should go at the top" } };
+
+
+
+            var comparer = new HalPropertyComparer();
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ContractResolver = new OrderedContractResolver(comparer),
+
+            };
+
+            var json = JsonConvert.SerializeObject(values, Formatting.Indented, new OrderedExpandoObjectConverter(comparer));
+            Console.WriteLine(json);
+
+            var thing = new
+            {
+                zed = "bar",
+                yak = 12,
+                wit = new DateTime(2001, 2, 3, 4, 5, 6),
+                xan = new DateTime(2001, 2, 3, 4, 5, 6),
+                _embedded = new[] {
+                    new { text = "embedded data" },
+                    new { text = "should appear" },
+                    new { text = "...at the end" }
+                },
+                cod = new DateTime(2001, 2, 3, 4, 5, 6),
+                bob = new DateTime(2001, 2, 3, 4, 5, 6),
+                alf = 123.456m,
+                _links = new { self = new { href = "this should go at the top" } }
+            };
+
+
+            var jsonThing = JsonConvert.SerializeObject(thing, Formatting.Indented, new OrderedExpandoObjectConverter(comparer));
+            Console.WriteLine(jsonThing);
+            //dynamic fnord = new ExpandoObject();
+            //fnord.zed = "bar";
+            //fnord.yak = 12;
+            //fnord.wit = new DateTime(2001, 2, 3, 4, 5, 6, 7);
+            //fnord.cod = "look! codfish!";
+            //fnord._links = new { self = new { href = "http://www.foo.com/bar/" } };
+            //fnord._embedded = new { data = new[] { new { foo = "bar" }, new { foo = "baz" } } };
+
+            //JsonConvert.DefaultSettings = () => new JsonSerializerSettings() {
+            //    ContractResolver = new OrderedContractResolver()
+            //};
+
+            //var json = JsonConvert.SerializeObject(thing, Formatting.Indented);
+            //Console.WriteLine(json);
+
+            //var bson = JsonConvert.SerializeObject(fnord, Formatting.Indented);
+            //Console.WriteLine(bson);
 
             //PerformanceTests t = new PerformanceTests();
             //t.DeserializeLargeJson();
 
             //PerformanceTests t = new PerformanceTests();
-            TokenWriteToAsync();
+            //TokenWriteToAsync();
             //t.Iterations = 50000;
             //t.BenchmarkDeserializeMethod<TestClass>(PerformanceTests.SerializeMethod.JsonNet, PerformanceTests.JsonText);
 
